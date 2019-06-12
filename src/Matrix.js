d58f375208c39d227a8680a3110c6a2329beb472
github username <github email address>
2019-06-12 11:22:51 -0400

    use cell componet replace getRow's map to

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class Matrix extends Component {
 
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    // vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map(val => <Cell value={val} />) 
+
+
   )
 
   genMatrix = () => (
@@ -18,7 +21,7 @@ export default class Matrix extends Component {
     )
   }
 
-  Matrix.defaultProps = {
-
-  }
+  // Matrix.defaultProps = {
+  //
+  // }
 }
