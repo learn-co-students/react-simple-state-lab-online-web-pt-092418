9f13bb55ef63bb8fad750681b44197ac90a110ff
github username <github email address>
2019-06-12 15:12:45 -0400

    add defaultProps

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import Cell from './Cell';
+import { defaultVal } from './data.js';
 
 export default class Matrix extends Component {
 
   genRow = (vals) => (
     // vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-    vals.map(val => <Cell value={val} />) 
-
-
+    vals.map(val => <Cell value={val} />)
   )
 
   genMatrix = () => (
@@ -20,8 +20,8 @@ export default class Matrix extends Component {
       </div>
     )
   }
+}
 
-  // Matrix.defaultProps = {
-  //
-  // }
+Matrix.defaultProps = {
+  values: defaultVal
 }
