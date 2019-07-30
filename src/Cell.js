2cd92cf1abf4f69f06fd6316829a687620ea6da8
github username <github email address>
2019-07-30 00:04:13 -0400

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: this.props.value
+        }
+
+    }
+
+    change = () => {
+        const newColor = "#333";
+        this.setState({
+            color: newColor
+        })
+    };
+
+
+    render() {
+        return (
+            <div className="cell"
+            onClick={this.change}
+            style={{backgroundColor: this.state.color}}>
+            </div>
+        )
+    }
+}
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import Cell from './Cell.js'
+import {defaultVal} from './data.js'
 
 export default class Matrix extends Component {
   
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
+    vals.map(val => <Cell value={val}/>) // replace me and render a cell component instead!
+  );
   
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
+  );
   
   render() {
     return (
@@ -17,5 +19,8 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+}
+
+Matrix.defaultProps = {
+  values: defaultVal
 }
